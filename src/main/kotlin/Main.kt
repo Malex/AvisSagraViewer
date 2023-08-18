aba3315d432795da708055585234e6e168e182e8
Alex Marcat <alex.marcat@gft.com>
2023-08-18 22:03:38 +0200

    Add reset button

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -14,7 +14,6 @@ import androidx.compose.ui.window.application
 @Composable
 @Preview
 fun App() {
-    val text = "Aggiorna"
 
     var results by remember { mutableStateOf(listOf<List<String>>()) }
     val filters = mutableStateListOf<String>()
@@ -23,11 +22,23 @@ fun App() {
 
     MaterialTheme {
         Column {
-            Button(onClick = {
-                results = if (filters.isEmpty()) queryExecutor.getBasic() else queryExecutor.getSpecificProductsbyName(filters)
-            }, colors = ButtonDefaults.buttonColors(Color.Gray),
-                modifier = Modifier.padding(10.dp)) {
-                Text(text)
+            Row {
+                Button(onClick = {
+                    results = if (filters.isEmpty()) queryExecutor.getBasic() else queryExecutor.getSpecificProductsbyName(filters)
+                }, colors = ButtonDefaults.buttonColors(Color.Gray),
+                    modifier = Modifier.padding(10.dp)) {
+                    Text("Aggiorna")
+                }
+
+                Button(
+                    onClick = {
+                        filters.removeAll { true }
+                        results = queryExecutor.getBasic()
+                    }, colors = ButtonDefaults.buttonColors(Color.Gray),
+                    modifier = Modifier.padding(10.dp)) {
+                    Text("Reset")
+                }
+
             }
             TableScreen(
                 listOf("Categoria", "Piatto", "Quantità"),
